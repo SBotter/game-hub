780fb2e7275862e395e143fc641f06c8f677128f
Sergio Botter <sergio.botter@gmail.com>
2023-11-26 19:52:22 -0800

    built game cards

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside"></GridItem>
+        <GridItem area="aside">aside</GridItem>
       </Show>
       <GridItem area="main">
         <GameGrid />
