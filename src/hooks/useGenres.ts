39754fa2c8f6fd89e2986c68e75853ae3144486c
Sergio Botter <sergio.botter@gmail.com>
2023-12-02 22:44:26 -0800

    Refactoring Genres and Plaform with ReactQuery

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,8 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
+import genres from "../data/genres"
+
 
 export interface Genre{
     id: number;
@@ -6,6 +10,15 @@ export interface Genre{
     image_background: string;
 }
 
-const useGenres = () => useData<Genre>('/genres') ;
+const useGenres = () => {
+    return useQuery({
+        queryKey: ['genres'],
+        queryFn: () => apiClient
+            .get<FetchResponse<Genre>>('/genres')
+            .then(res => res.data),
+        staleTime: 24 * 60 * 60 * 1000,
+        initialData: { count: genres.length, results: genres }
+    });
+}
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,22 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import useData, { FetchResponse } from "./useData";
+import apiClient from "../services/api-client";
+import platforms from "../data/platforms";
 
-export interface Platform{
-    id: number;
-    name: string;
-    slug: string;
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
 }
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents')
-
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("platform/lists/parents")
+        .then((resp) => resp.data),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: { count: platforms.length, results: platforms },
+  });
 export default usePlatforms;
