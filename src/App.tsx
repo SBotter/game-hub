45311856feaf371cfce204b31e2ea81a2d649457
Sergio Botter <sergio.botter@gmail.com>
2023-12-03 21:27:19 -0800

    Create lookup hooks

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
-import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
-import { Platform } from "./hooks/usePlatforms";
 import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
 import APIClient from "../services/api-client";
 
-const apiClient = new APIClient<Platform>("platform/lists/parents");
+const apiClient = new APIClient<Platform>("platforms/lists/parents");
 
 export interface Platform {
   id: number;
@@ -15,6 +15,6 @@ const usePlatforms = () =>
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
     staleTime: 24 * 60 * 60 * 1000,
-    initialData: { count: platforms.length, results: platforms },
+    initialData: platforms,
   });
 export default usePlatforms;
