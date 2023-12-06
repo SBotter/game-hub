8f830e4f175615bd6f462d16ff7c41dbe9385ec8
Sergio Botter <sergio.botter@gmail.com>
2023-12-05 21:42:46 -0800

    setting the time on staleTime

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,4 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
 import APIClient from "../services/api-client";
 import genres from "../data/genres";
 
@@ -14,7 +15,7 @@ const useGenres = () => {
   return useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000,
+    staleTime: ms("24h"),
     initialData: genres,
   });
 };
