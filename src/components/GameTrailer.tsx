b29708c1d0e923ec1cc6055f3f57d3257f4f2e2c
Sergio Botter <sergio.botter@gmail.com>
2023-12-07 00:18:57 -0800

    Build Game Trailer

---
+++ src/components/GameTrailer.tsx
@@ -0,0 +1,19 @@
+import useTrailers from "../hooks/useTrailers";
+
+interface Props {
+  gameId: number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, isLoading, error } = useTrailers(gameId);
+
+  if (isLoading) return null;
+  if (error) throw error;
+
+  const first = data?.results[0];
+  return first ? (
+    <video src={first.data[480]} poster={first.priview} controls />
+  ) : null;
+};
+
+export default GameTrailer;
---
+++ src/entities/Trailer.ts
@@ -0,0 +1,9 @@
+export interface Trailer {
+  id: number;
+  name: string;
+  priview: string;
+  data: {
+    480: string;
+    max: string;
+  };
+}
