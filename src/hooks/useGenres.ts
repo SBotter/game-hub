d48f99fab210cff0d3b295bfcb1bf8da33387ecf
Sergio Botter <sergio.botter@gmail.com>
2023-12-06 23:19:25 -0800

    Create entity interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -2,14 +2,9 @@ import { useQuery } from "@tanstack/react-query";
 import ms from "ms";
 import APIClient from "../services/api-client";
 import genres from "../data/genres";
+import { Genre } from "../entities/Genre";
 
 const apiClient = new APIClient<Genre>("/genres");
-
-export interface Genre {
-  id: number;
-  name: string;
-  image_background: string;
-}
 
 const useGenres = () => {
   return useQuery({
